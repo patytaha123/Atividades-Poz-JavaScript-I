04f5f46a817ed85e8e0d2e175bda5d2166c71ef4
PATRICIA TAHA <107588085+patytaha123@users.noreply.github.com>
2023-12-07 19:24:49 -0300

    Add files via upload
    "Adiconando  OneCompiler das quatro operações básicas"

---
+++ index.js
@@ -0,0 +1,17 @@
+function operacoesBasicas(numero1, numero2) {
+    // Adição
+    console.log(`${numero1} + ${numero2} = ${numero1 + numero2}`);
+    
+    // Subtração
+    console.log(`${numero1} - ${numero2} = ${numero1 - numero2}`);
+    
+    // Multiplicação
+    console.log(`${numero1} x ${numero2} = ${numero1 * numero2}`);
+    
+    // Divisão
+    console.log(`${numero1} / ${numero2} = ${numero1 / numero2}`);
+}
+
+
+// Exemplo de uso da função
+operacoesBasicas(4, 5);
